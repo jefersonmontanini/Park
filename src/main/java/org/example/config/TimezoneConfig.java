d95ece90d97627628ab16a4692779b080387ab00
unknown <dev.jeferson@gmail.com>
2023-11-07 16:20:09 -0300

    set_Timezone

--- src/main/java/org/example/config/TimezoneConfig.java
+++ src/main/java/org/example/config/TimezoneConfig.java
@@ -1,2 +1,13 @@
-package org.example.config;public class TimezoneConfig {
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimezoneConfig {
+
+    public void timezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
