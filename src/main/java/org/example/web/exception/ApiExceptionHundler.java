4fa236d90afb18be01d2373284ebeed4bfea89f0
unknown <dev.jeferson@gmail.com>
2023-11-13 11:33:05 -0300

    features_exceptionHandlers

---
+++ src/main/java/org/example/web/exception/ApiExceptionHundler.java
@@ -0,0 +1,24 @@
+package org.example.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHundler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> MethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request, BindingResult result) {
+        log.error("Api Error -", exception);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campos(s) invalido(s)", result ));
+    }
+}
---
+++ src/main/java/org/example/web/exception/ErrorMessage.java
@@ -0,0 +1,51 @@
+package org.example.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @ToString
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusMessage;
+    private String errorMessage;
+    private Map<String, String> errors;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.errorMessage = message;
+    }
+
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.errorMessage = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+        for (FieldError fieldError: result.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
